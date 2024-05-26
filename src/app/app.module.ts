205e24adf121c5eec3f26a83299f115cea5a6ce1
Nizar aoun <113100482+Nizaraoun@users.noreply.github.com>
2024-05-26 17:37:58 +0100

    aqszq

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastModule } from 'primeng/toast';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
@@ -17,28 +17,19 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { DropdownModule } from 'primeng/dropdown';
 import { MatIconModule } from '@angular/material/icon';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    
-  ],
-  imports: [
-    DropdownModule,  // Add PrimeNG DropdownModule here
-    BrowserModule,
-    AppRoutingModule,
-    CardModule,
-    InputTextModule,
-    ReactiveFormsModule,
-    ButtonModule,
-    HttpClientModule,
-    FormsModule , // Add this line
-    ToastModule,
-    BrowserAnimationsModule,
-
-  ],
-  providers: [MessageService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+    ],
+    bootstrap: [AppComponent], imports: [DropdownModule, // Add PrimeNG DropdownModule here
+        BrowserModule,
+        AppRoutingModule,
+        CardModule,
+        InputTextModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        FormsModule, // Add this line
+        ToastModule,
+        BrowserAnimationsModule], providers: [MessageService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
